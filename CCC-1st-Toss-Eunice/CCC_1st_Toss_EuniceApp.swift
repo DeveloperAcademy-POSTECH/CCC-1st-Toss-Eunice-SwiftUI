b32ff04d8dfbdc717e929b9e8c18814e96cfbfc4
EuniceNam <caellime@gmail.com>
2022-06-29 15:35:59 +0900

    Initial Commit

---
+++ CCC-1st-Toss-Eunice/CCC_1st_Toss_EuniceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCC_1st_Toss_EuniceApp.swift
+//  CCC-1st-Toss-Eunice
+//
+//  Created by Hyorim Nam on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct CCC_1st_Toss_EuniceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCC-1st-Toss-Eunice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CCC-1st-Toss-Eunice
+//
+//  Created by Hyorim Nam on 2022/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
